d5750380677cd6e7cef62bedc48d8a879b96fd63
Anton Degnera <antilik@gmail.com>
2021-07-19 00:09:26 +0300

    Steps, Pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,28 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let countChar = 0;
+  const width = n * 2 - 1;
+  const printMiddle = (count) => "#".repeat(count);
+  const diffStart = (char) =>
+    width - char.length ? (width - char.length) / 2 + char.length : 0; // (5 - 1)/2 = 2
+  const printStart = (specialChars) =>
+    specialChars.padStart(diffStart(specialChars));
+  const printEnd = (chars) => chars.padEnd(width);
+
+  for (let i = 0; i < n; i++) {
+    if (i === 0) {
+      countChar += 1;
+    } else {
+      countChar += 2;
+    }
+
+    const middle = printMiddle(countChar);
+    const beginning = printStart(middle);
+    const end = printEnd(beginning);
+    console.log(end);
+  }
+}
 
 module.exports = pyramid;
--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// First approach
+function steps(n) {
+  const helper = (count, char) => new Array(count).fill(char);
+
+  for (let i = 0; i < n; i++) {
+    console.log([...helper(i + 1, "#"), ...helper(n - i - 1, " ")].join(""));
+  }
+}
+
+// Second approach
+// function steps(n) {
+//   if (n <= 0) {
+//     return;
+//   }
+//   const getString = (count) => "#".repeat(count).padEnd(n);
+
+//   for (let i = 0; i < n; i++) {
+//     console.log(getString(i + 1));
+//   }
+// }
+
+// Third approach
+// function steps(n) {
+//   const getArrWithChar = (count, char) =>
+//     count ? new Array(count).fill(char) : [];
+
+//   printIt(n);
+//   function printIt(n, m = 1) {
+//     if (n === 0) {
+//       return;
+//     }
+
+//     console.log(
+//       [...getArrWithChar(m, "#"), ...getArrWithChar(n - 1, " ")].join(""),
+//     );
+//     printIt(n - 1, m + 1);
+//   }
+// }
 
 module.exports = steps;
