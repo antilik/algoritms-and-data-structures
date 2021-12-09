ff2ff6e81dae9c1f81f33e5fc41791d43d747360
Anton Degnera <antilik@gmail.com>
2021-12-09 10:38:36 +0200

    Added more solutions for Pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -6,33 +6,68 @@
 // --- Examples
 //   pyramid(1)
 //       '#'
-//   pyramid(2)
+// pyramid(2);
 //       ' # '
 //       '###'
-//   pyramid(3)
+// pyramid(4);
 //       '  #  '
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  let countChar = 1;
-  const width = n * 2 - 1;
-  const printMiddle = (count) => "#".repeat(count);
-  const diffStart = (charLength) =>
-    width - charLength ? (width - charLength) / 2 + charLength : 0; // (5 - 1)/2 = 2
-  const printStart = (specialChars) =>
-    specialChars.padStart(diffStart(specialChars.length));
-  const printEnd = (startMiddle) => startMiddle.padEnd(width);
+// First solution
+// function pyramid(n) {
+//   let countChar = 1;
+//   const width = n * 2 - 1;
+//   const printMiddle = (count) => "#".repeat(count);
+//   const calculateDifference4Start = (charLength) =>
+//     width - charLength ? (width - charLength) / 2 + charLength : 0;
+//   const printStart = (specialChars) =>
+//     specialChars.padStart(calculateDifference4Start(specialChars.length));
+//   const printEnd = (startMiddle) => startMiddle.padEnd(width);
+//
+//   for (let i = 0; i < n; i++) {
+//     if (i > 0) {
+//       countChar += 2;
+//     }
+//     const middle = printMiddle(countChar);
+//     const beginningMiddle = printStart(middle);
+//     const allRow = printEnd(beginningMiddle);
+//     console.log(allRow);
+//   }
+// }
 
-  for (let i = 0; i < n; i++) {
-    if (i > 0) {
-      countChar += 2;
+// Second solution (better)
+// function pyramid(n) {
+//   const rowLength = n * 2 - 1;
+//   const getRow = (symbolQuantity, padLength) =>
+//     ' '.repeat(padLength) + '#'.repeat(symbolQuantity) + ' '.repeat(padLength);
+//
+//   for (let index = 1; index <= n; index++) {
+//     const specialQuantity = index * 2 - 1;
+//     const halfPadLength = Math.floor((rowLength - specialQuantity) / 2);
+//     console.log(getRow(specialQuantity, halfPadLength));
+//   }
+// }
+
+// Third solution
+function pyramid(n) {
+  const width = n*2 -1;
+  let counter = 1;
+
+  function printRow() {
+    const middle = counter*2 -1;
+    const halfOfEmptySpace = Math.floor((width - middle)/2);
+    const halfOfEmptySpaceSymbols = (middle === width) ? "" : " ".repeat(halfOfEmptySpace);
+    const row = halfOfEmptySpaceSymbols + '#'.repeat(middle) + halfOfEmptySpaceSymbols;
+    console.log(row);
+
+    if (counter !== n) {
+      counter +=1;
+      printRow()
     }
-    const middle = printMiddle(countChar);
-    const beginningMiddle = printStart(middle);
-    const allRow = printEnd(beginningMiddle);
-    console.log(allRow);
   }
+  (n >0 ) && printRow();
 }
+
 
 module.exports = pyramid;
