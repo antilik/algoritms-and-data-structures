76f3b5dcb64d201168a79c0d06fd4129c410c7ca
Anton Degnera <antilik@gmail.com>
2021-12-11 01:21:41 +0200

    Added more solutions.

--- vowels/index.js
+++ vowels/index.js
@@ -3,17 +3,40 @@
 // used in a string.  Vowels are the characters 'a', 'e'
 // 'i', 'o', and 'u'.
 // --- Examples
-//   vowels('Hi There!') --> 3
-//   vowels('Why do you ask?') --> 4
-//   vowels('Why?') --> 0
+//   vowels('Hi There!') // --> 3
+//   vowels('Why do you ask?'); // --> 4
+//   vowels('Why?'); // --> 0
 
+// First solution
+// function vowels(str) {
+//   return !str.length ? 0 : str
+//     .toLowerCase()
+//     .split('')
+//     .reduce((acc, char) => {
+//    return (['a','e','i','o','u'].includes(char)) ? acc += 1 : acc;
+//   }, 0);
+// }
+
+// Second solution
+// function vowels(str) {
+//   let count = 0;
+//   for (const char of str.toLowerCase()) {
+//     ['a','e','i','o','u'].includes(char) ? count++ : count;
+//   }
+//   return count;
+// }
+// function vowels(str) {
+//   let count = 0;
+//   str.toLowerCase().split('').forEach((char) => {
+//     ['a', 'e', 'i', 'o', 'u'].includes(char) ? count++ : count;
+//   });
+//   return count;
+// }
+
+// Third solution
 function vowels(str) {
-  return !str.length ? 0 : str
-    .toLowerCase()
-    .split('')
-    .reduce((acc, letter) => {
-   return (['a','e','i','o','u'].includes(letter)) ? acc += 1 : acc;
-  }, 0);
+  const matches = str.match(/[aeiou]/gi);
+  return matches?.length || 0;
 }
 
 module.exports = vowels;
